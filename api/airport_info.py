f9ce31312c6134da3c57b36f466a31c3b9eef558
Aadil <aadil.yunush@gmail.com>
2023-02-18 08:03:10 +0100

    added worktree

--- api/airport_info.py
+++ api/airport_info.py
@@ -2,6 +2,7 @@ import requests
 
 
 if __name__=="__main__":
+    
     headers = {
         'x-rapidapi-key': "ce19d0164fmsh3d383efc0e85ce5p16dcb1jsnb1a4a3c79541",
         'x-rapidapi-host': "airport-info.p.rapidapi.com"
